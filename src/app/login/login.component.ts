7f7197dc0fd4831574ec41015ee581302a9efdfe
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-01-13 14:54:52 -0500

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Auth } from 'aws-amplify'
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onLogin() {
+
+  }
+
+}
