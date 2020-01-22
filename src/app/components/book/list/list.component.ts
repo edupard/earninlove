668b556551a0d8ba0df0bdc796121df09258eda2
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-01-21 20:24:27 -0500

    test

--- src/app/components/book/list/list.component.ts
+++ src/app/components/book/list/list.component.ts
@@ -22,17 +22,28 @@ export class ListComponent implements OnInit {
 
 
   items = [];
+  originalItems = [];
   public error = false;
   public progress = false;
   public itemText:string = '';
 
   constructor(private data: DataService) { }
 
+  public saveOriginalItems()
+  {
+    this.originalItems = this.items;
+  }
+
+  public hasChanges()
+  {
+    return this.originalItems !== this.items;
+  }
+
   ngOnInit() {
     this.progress = true;
     this.data.getData(this.ctrl)
     .subscribe(
-      data => { this.items = data.json === undefined ? []: data.json.items; this.error = false; },
+      data => { this.items = data.json === undefined ? []: data.json.items; this.error = false; this.saveOriginalItems(); },
       err => { this.error = true; }
     )
     .add(() => {
@@ -48,7 +59,7 @@ export class ListComponent implements OnInit {
     this.progress = true;
     this.data.setData(this.ctrl, { items: this.items})
     .subscribe(
-      data => { this.error = false; },
+      data => { this.error = false; this.saveOriginalItems(); },
       err => { this.error = true; }
     )
     .add(() => {
--- src/app/components/book/text-area/text-area.component.ts
+++ src/app/components/book/text-area/text-area.component.ts
@@ -10,17 +10,23 @@ export class TextAreaComponent implements OnInit {
 
   @Input('ctrl') ctrl: string;
 
-  public text;
+  public text = '';
+  public originalText = '';
   public error = false;
   public progress = false;
 
   constructor(private data: DataService) { }
 
+  public saveOriginalText()
+  {
+    this.originalText = this.text;
+  }
+
   ngOnInit() {
     this.progress = true;
     this.data.getData(this.ctrl)
     .subscribe(
-      data => { this.text = data.json === undefined ? "": data.json.text; this.error = false; },
+      data => { this.text = data.json === undefined ? "": data.json.text; this.error = false; this.saveOriginalText(); },
       err => { this.error = true; }
     )
     .add(() => {
@@ -32,12 +38,17 @@ export class TextAreaComponent implements OnInit {
     this.progress = true;
     this.data.setData(this.ctrl, { text: this.text})
     .subscribe(
-      data => { this.error = false; },
+      data => { this.error = false; this.saveOriginalText(); },
       err => { this.error = true; }
     )
     .add(() => {
       this.progress = false;
     });
+  }
+
+  public hasChanges()
+  {
+    return this.originalText !== this.text;
   }
 
 }
